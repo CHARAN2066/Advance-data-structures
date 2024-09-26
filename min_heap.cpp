e43b7828b1722cc281ba5b0a11a489c2095d29d4
CHARAN2066 <147472977+CHARAN2066@users.noreply.github.com>
2024-09-26 21:16:06 +0530

    Add files via upload

---
+++ min_heap.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Function to swap two elements
+void swap(int *a,int *b)
+{
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+
+// Function to heapify up (maintain max heap property)
+void heapify_up(int min_heap[],int index)
+{
+    int parent_index=(index-1)/2;
+
+    // If the current element is lesser than its parent, swap them
+    if(index>0&&(min_heap[index]<min_heap[parent_index]))
+    {
+        swap(&min_heap[index],&min_heap[parent_index]);
+        heapify_up(min_heap,parent_index);
+    }
+}
+
+
+
+// Function to heapify down (maintain max heap property)
+void heapify_down(int max_heap[],int *size,int index)
+{
+    int largest=index;
+    int left_child=2*index+1;
+    int right_child=2*index+2;
+    if(left_child<(*size)&&max_heap[left_child]<max_heap[largest])
+    largest=left_child;
+    else if(right_child>(*size)&&max_heap[right_child]<max_heap[largest])
+    largest=right_child;
+    if(largest!=index)
+    {
+        swap(max_heap[largest],max_heap[index]);
+        heapify_down(max_heap,size,largest);
+    }
+}
+
+
+// Function to insert an element into the max heap
+void insert(int val,int *size,int min_heap[])
+{
+    
+    min_heap[*(size)]=val;
+    (*size)++;
+    heapify_up(min_heap,*size-1);
+}
+
+
+
+// Function to delete the root element (maximum element) from the max heap
+void delete_the_element(int max_heap[],int *size)
+{
+    if((*size)<0)
+    cout<<"Heap is empty"<<endl;
+    else
+    {
+        int root=max_heap[0];
+        max_heap[0]=max_heap[(*size)-1];  // Replace root with the last element
+        (*size)--;
+        heapify_down(max_heap,size,0);    // Heapify down from the root
+    }
+}
+int main()
+{
+    int min_heap[1000];
+    int size=0;
+    int choice;
+    while (1)
+    {
+        cout<<"1.Insert the element"<<endl;
+        cout<<"2.Get the minimum in the element"<<endl;
+        cout<<"3. Delete the element"<<endl;
+        cin>>choice;
+        if(choice==1)
+        {
+            int val;
+            cin>>val;
+            insert(val,&size,min_heap);
+            cout<<"The element is inserted"<<endl;
+        }
+        else if(choice==2)
+        {
+            cout<<"the min is"<<(min_heap[0])<<endl;
+        }
+        else if(choice==3)
+        {
+            delete_the_element(min_heap,&size);
+            cout<<"The element is deleted"<<endl;
+        }
+        else
+        break;
+    }
+    // insert(10,&size,max_heap);
+}
